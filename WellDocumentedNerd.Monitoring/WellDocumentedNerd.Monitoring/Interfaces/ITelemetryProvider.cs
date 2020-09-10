c4bc7a9c482c2f981b161a94d1eafd348c7cf390
Kevin Mack <kevinmackdev@outlook.com>
2020-09-10 13:08:11 -0400

    Pushing initial commit

---
+++ WellDocumentedNerd.Monitoring/WellDocumentedNerd.Monitoring/Interfaces/ITelemetryProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WellDocumentedNerd.Monitoring.Interfaces
+{
+    public interface ITelemetryProvider
+    {
+        Guid CorrelationKey { get; set; }
+
+        void TrackDependency(string name, string typeName, string data, DateTime startTime, TimeSpan duration, bool success);
+        void TrackEvent(string name);
+        void TrackEvent(string name, Dictionary<string, string> properties);
+        void TrackEvent(string name, Dictionary<string, string> properties, Dictionary<string, double> metrics);
+        void TrackException(Exception ex);
+        void TrackMetric(string name, double value);
+    }
+}
---
+++ WellDocumentedNerd.Monitoring/WellDocumentedNerd.Monitoring/Providers/TelemetryProvider.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WellDocumentedNerd.Monitoring.Interfaces;
+
+namespace WellDocumentedNerd.Monitoring.Providers
+{
+    public class TelemetryProvider : ITelemetryProvider
+    {
+        private TelemetryClient _client;
+        public Guid CorrelationKey { get; set; }
+
+        public TelemetryProvider()
+        {
+            _client = new TelemetryClient(TelemetryConfiguration.CreateDefault());
+            CorrelationKey = Guid.NewGuid();
+        }
+        public void TrackEvent(string name)
+        {
+            var properties = this.AddCorrelationKey();
+            _client.TrackEvent(name, properties);
+        }
+
+        public void TrackEvent(string name, Dictionary<string, string> properties)
+        {
+            properties = this.AddCorrelationKey(properties);
+            _client.TrackEvent(name, properties);
+        }
+
+        public void TrackEvent(string name, Dictionary<string, string> properties, Dictionary<string, double> metrics)
+        {
+            properties = this.AddCorrelationKey(properties);
+            _client.TrackEvent(name, properties, metrics);
+        }
+
+        public void TrackMetric(string name, double value)
+        {
+            var properties = this.AddCorrelationKey();
+            _client.TrackMetric(name, value, properties);
+        }
+
+        public void TrackException(Exception ex)
+        {
+            var properties = this.AddCorrelationKey();
+            _client.TrackException(ex, properties);
+        }
+
+        public void TrackDependency(string name, string typeName, string data, DateTime startTime, TimeSpan duration, bool success)
+        {
+            _client.TrackDependency(typeName, name, data, startTime, duration, success);
+        }
+
+        private Dictionary<string, string> AddCorrelationKey()
+        {
+            var properties = new Dictionary<string, string>();
+            return this.AddCorrelationKey(properties);
+        }
+
+        private Dictionary<string, string> AddCorrelationKey(Dictionary<string, string> properties)
+        {
+            properties.Add("CorrelationKey", CorrelationKey.ToString());
+            return properties;
+        }
+    }
+}
