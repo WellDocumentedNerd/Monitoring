7df153517706c420097274500004b6306f43275a
Kevin Mack <kevinmackdev@outlook.com>
2020-09-10 13:12:40 -0400

    Initial checkin

---
+++ WellDocumentedNerd.Random/WellDocumentedNerd.Random/Interfaces/IRandomErrorProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace WellDocumentedNerd.Random.Interfaces
+{
+    public interface IRandomErrorProvider
+    {
+        bool ReturnError(int min, int max, int threshold);
+    }
+}
---
+++ WellDocumentedNerd.Random/WellDocumentedNerd.Random/Providers/RandomErrorProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WellDocumentedNerd.Random.Interfaces;
+
+namespace WellDocumentedNerd.Random.Providers
+{
+    public class RandomErrorProvider : IRandomErrorProvider
+    {
+        public bool ReturnError(int min, int max, int threshold)
+        {
+            System.Random r = new System.Random();
+            int genRand = r.Next(min, max);
+
+            if (genRand <= threshold)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
